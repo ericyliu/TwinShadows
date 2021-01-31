7606d51f9bbdcc6e96d61191c75fc9d8918b0aa4
Eric Liu <eyliubit@gmail.com>
2021-01-30 22:51:07 -0800

    add house models

--- Assets/Scripts/StructureDecorator.cs
+++ Assets/Scripts/StructureDecorator.cs
@@ -1,32 +1,72 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Google.Maps.Event;
 
 public class StructureDecorator : MonoBehaviour
 {
   public GameObject player;
+  public List<GameObject> houseModels = new List<GameObject>();
+
+  List<GameObject> roads = new List<GameObject>();
+  List<GameObject> houses = new List<GameObject>();
+
 
   public void OnDidCreateExtrudedStructure(DidCreateExtrudedStructureArgs args)
   {
-    args.GameObject.AddComponent<MeshCollider>();
+    CreateRandomHouse(args.GameObject);
   }
 
   public void OnDidCreateModeledStructure(DidCreateModeledStructureArgs args)
   {
-    args.GameObject.AddComponent<MeshCollider>();
-  }
-
-  public void OnDidCreateRegion(DidCreateRegionArgs args)
-  {
-    args.GameObject.AddComponent<MeshCollider>();
+    CreateRandomHouse(args.GameObject);
   }
 
   public void OnDidCreateSegment(DidCreateSegmentArgs args)
   {
-    args.GameObject.AddComponent<MeshCollider>();
+    roads.Add(args.GameObject);
   }
 
   public void OnStart(MapLoadStartArgs args)
   {
     player.transform.position = new Vector3(player.transform.position.x, 10f, player.transform.position.z);
+  }
+
+  public void OnLoaded(MapLoadedArgs args)
+  {
+    foreach (GameObject house in houses)
+    {
+      Debug.Log(roads.Count);
+      GameObject closestRoad = GetClosestRoad(house);
+      Debug.Log(house);
+      Debug.Log(closestRoad);
+      house.transform.LookAt(closestRoad.transform);
+    }
+  }
+
+  void CreateRandomHouse(GameObject parent)
+  {
+    GameObject house = GameObject.Instantiate(houseModels[Random.Range(0, houseModels.Count)], parent.transform);
+    house.transform.localScale = new Vector3(30, 30, 30);
+    parent.GetComponent<MeshRenderer>().enabled = false;
+    houses.Add(house);
+  }
+
+  GameObject GetClosestRoad(GameObject house)
+  {
+    GameObject bestTarget = null;
+    float closestDistanceSqr = Mathf.Infinity;
+    Vector3 currentPosition = house.transform.position;
+    foreach (GameObject potentialTarget in roads)
+    {
+      Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+      float dSqrToTarget = directionToTarget.sqrMagnitude;
+      if (dSqrToTarget < closestDistanceSqr)
+      {
+        closestDistanceSqr = dSqrToTarget;
+        bestTarget = potentialTarget;
+      }
+    }
+
+    return bestTarget;
   }
 }
